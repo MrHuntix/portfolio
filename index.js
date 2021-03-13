764554215a719d5e7ccee8c5e64963cd87b00628
Puneeth <puntirock717@gmail.com>
2021-03-13 19:23:12 +0530

    added sse support for realtime project update

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
-let projects = {};
+let projects = [];
+let projectList = null;
+let projectDetails = null;
+//./chrome.exe --user-data-dir="C:/Chrome dev session" --disable-web-security
+
+let settings = {
+    "url": "http://localhost:3100/client",
+    "method": "GET",
+    "timeout": 0,
+    "headers": {
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InB1bmVldGgiLCJpYXQiOjE2MDY4NDcxNTJ9.A-1MfTqDxSgqm-5_Js4m3SBrb8du2-csMC-Er-h022s",
+    },
+};
+
+let sseSource = new EventSource('http://localhost:3100/client/delta');
+
+sseSource.onopen = function () {
+    console.log('sse opened');
+}
+
+sseSource.onerror = function (err) {
+    console.log(`sse error ${err.eventPhase}`);
+}
+
+sseSource.onmessage = function (event) {
+    console.log(`event data ${JSON.parse(event.data)}::${projects.length}`);
+    projects.push(JSON.parse(event.data));
+    if (projects.length == 1) {
+        updateDetails(projectDetails === null ? $("#details") : projectDetails, 0);
+
+    } else {
+        console.log(`added a new project::${projects.length}`);
+    }
+    let latestProject = projects[projects.length - 1];
+    var caroselItem = $(`<div id=${["app_name"]}></div>`);
+    if (projects.length === 1)
+        caroselItem.addClass("carousel-item active");
+    else
+        caroselItem.addClass("carousel-item");
+    var caroselCaption = $('<div class="card bg-secondary"></div>');
+    var heading = $(`<div class="card-header caption"><h3>${latestProject["app_name"]}</h3></div>`);
+    var description = $(`<div class="card-body caption"><p>${latestProject["description"]}</p></div>`);
+    // var button = $("<button>details</button>");
+    caroselCaption.append(heading, description);
+    caroselItem.append(caroselCaption);
+    projectList.append(caroselItem);
+}
 
 let display = (index) => {
     let project = projects[index];
@@ -6,36 +52,36 @@ let display = (index) => {
 };
 
 $(document).ready(function () {
-    let projectList = $("#projectList");
-    let projectDetails = $("#details");
+    projectList = $("#projectList");
+    projectDetails = $("#details");
 
-    $.ajaxSetup({
-        async: false
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        if (response != null && response["apps"].length > 0) {
+            projects = response["apps"];
+            console.log(projects);
+            updateDetails(projectDetails, 0);
+            projects.forEach((project, index) => {
+                var caroselItem = $(`<div id=${project["app_name"]}></div>`);
+                if (index === 0)
+                    caroselItem.addClass("carousel-item active");
+                else
+                    caroselItem.addClass("carousel-item");
+                var caroselCaption = $('<div class="card bg-secondary"></div>');
+                var heading = $(`<div class="card-header caption"><h3>${project["app_name"]}</h3></div>`);
+                var description = $(`<div class="card-body caption"><p>${project["description"]}</p></div>`);
+                // var button = $("<button>details</button>");
+                caroselCaption.append(heading, description);
+                caroselItem.append(caroselCaption);
+                projectList.append(caroselItem);
+            });
+        } else {
+            projectDetails.empty();
+            projectDetails.append(`<p class="lead">There are no projects available</p>`);
+        }
     });
 
-    $.getJSON("/assets/projects.json", (data) => {
-        projects = data;
-        updateDetails(projectDetails, 0)
-    });
 
-    $.ajaxSetup({
-        async: true
-    });
-
-    projects.forEach((project, index) => {
-        var caroselItem = $(`<div id=${project["name"]}></div>`);
-        if (index === 0)
-            caroselItem.addClass("carousel-item active");
-        else
-            caroselItem.addClass("carousel-item");
-        var caroselCaption = $('<div class="card bg-secondary"></div>');
-        var heading = $(`<div class="card-header caption"><h3>${project["name"]}</h3></div>`);
-        var description = $(`<div class="card-body caption"><p>${project["description"]}</p></div>`);
-        // var button = $("<button>details</button>");
-        caroselCaption.append(heading, description);
-        caroselItem.append(caroselCaption);
-        projectList.append(caroselItem);
-    });
 
     $(window).scroll(function () {
         if ($(window).scrollTop() > 10) {
@@ -53,8 +99,8 @@ $(document).ready(function () {
 function updateDetails(element, index) {
     let project = projects[index];
     element.empty();
-    element.append(`<p class="lead">${project["name"]}<br/>${project["description"]}</p><br/>`);
-    element.append(`<p class="lead">Tech Stack: ${project["languages"].join()}</p><br/>`);
-    element.append(`<p class="lead">Links: <a href="${project["code"]}" data-toggle="tooltip" title="open in github">github</a>${project["live"] == "no" ? '' : ` | <a href="${project["live"]}" data-toggle="tooltip" title="try the live version">try</a>`}</p>`)
+    element.append(`<p class="lead">${project["app_name"]}<br/>${project["description"]}</p><br/>`);
+    element.append(`<p class="lead">Tech Stack: ${project["languages"]}</p><br/>`);
+    element.append(`<p class="lead">Links: <a href="${project["git_url"]}" data-toggle="tooltip" title="open in github">github</a>${project["live_url"] == "no" ? '' : ` | <a href="${project["live_url"]}" data-toggle="tooltip" title="try the live version">link</a>`}</p>`)
 
 }
